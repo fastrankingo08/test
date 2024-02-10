5c7fe4ad58ad53e82d0094859cef229ca502c014
Arif Khan <Arif@sparta.com>
2024-02-10 17:02:59 +0000

    added

---
+++ arif.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is latest content</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum qui rerum sit vero laudantium! Nulla mollitia accusantium iste. Dolorum, nesciunt veniam. Soluta aperiam animi ad suscipit optio maxime eum dolorum?</p>
+</body>
+</html>
