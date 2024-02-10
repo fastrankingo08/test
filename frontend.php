1f89868de95d6647e39971a27b472c9af9d22e33
Arif Khan <Arif@sparta.com>
2024-02-10 14:08:49 +0000

    add

---
+++ frontend.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frontend</title>
+</head>
+<body>
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius blanditiis culpa corporis esse iure corrupti nam impedit unde expedita architecto eveniet vel illum fugit rerum asperiores quaerat, dolore voluptas autem?</p>
+    </div>
+</body>
+</html>
